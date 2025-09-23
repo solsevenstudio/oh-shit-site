ce79813b0080e57b0704a7583dcc6f54ef3f69ca
David Barrow <david@solsevenstudio.com>
2025-09-23 21:30:52 +0100

    feat: switch subscribe form to Mailchimp

--- src/SubscribeForm.jsx
+++ src/SubscribeForm.jsx
@@ -1,3 +1,10 @@
+import React from "react";
+
+/**
+ * Mailchimp subscribe form
+ * Action URL comes from your embed:
+ * https://solsevenstudio.us20.list-manage.com/subscribe/post?u=defbb93410f7ef85725de88b1&id=0520fdc9c0&f_id=00807deef0
+ */
 export default function SubscribeForm() {
   return (
     <form
@@ -10,10 +17,22 @@ export default function SubscribeForm() {
       <input
         type="email"
         name="EMAIL"
+        id="mce-EMAIL"
         required
         placeholder="you@email.com"
         className="w-full sm:w-auto flex-grow rounded-xl px-3 py-2 bg-white/10 border border-white/20 text-white"
       />
+
+      {/* Bot trap (Mailchimp’s honeypot) */}
+      <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
+        <input
+          type="text"
+          name="b_defbb93410f7ef85725de88b1_0520fdc9c0"
+          tabIndex="-1"
+          defaultValue=""
+        />
+      </div>
+
       <button
         type="submit"
         className="rounded-xl px-6 py-2 bg-amber-400 text-[#0e2a2f] font-semibold hover:bg-amber-300"
